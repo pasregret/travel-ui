b0c416ce9c9b4d314c82927302e5d3cc38c71abe
dingxindong <1477015648@qq.com>
2019-12-09 22:57:25 +0800

    11

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/edu/teacher/'
+export default {
+
+  getPageList(page, limit, pageQuery) {
+    return request({
+      url: `${api_name}/pageQuery/${page}/${limit}`,
+      method: 'get',
+      params: pageQuery
+    })
+  },
+  removeById(id) {
+    return request({
+      url: `${api_name}/${id}`,
+      method: 'delete'
+    })
+  },
+  save(teacher) {
+    return request({
+      url: api_name,
+      method: 'post',
+      data: teacher
+    })
+  },
+  getById(id) {
+    return request({
+      url: `${api_name}/${id}`,
+      method: 'get'
+    })
+  },
+  updateById(teacher) {
+    return request({
+      url: `${api_name}/${teacher.id}`,
+      method: 'put',
+      data: teacher
+    })
+  }
+}
